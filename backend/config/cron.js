6f890f748c499c27ba91a3ed7e8369eed595a3ed
utsav173 <khatriutsav40@gmail.com>
2023-10-18 21:07:51 +0530

    add cron to keep server up

--- backend/config/cron.js
+++ backend/config/cron.js
@@ -6,4 +6,14 @@ module.exports.cron = {
       await sails.helpers.resetlimit();
     },
   },
+  secondJob:{
+    // run after every 13 minutes
+    schedule: '13 * * * * *',
+    onTick: function () {
+      // get current date and time and ip address
+      const dateTime = new Date().toLocaleTimeString();
+      const dateDay = new Date().toLocaleDateString();
+      console.log(`Date: ${dateDay} Time: ${dateTime}`);
+    }
+  }
 };
